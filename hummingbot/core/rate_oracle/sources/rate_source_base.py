bf20fbadcdd105ada7bed10bf85e063eb33de361
21Dipti <diptimahakalkar21@gmail.com>
2026-03-06 20:51:14 +0530

    Refactor rate source classes to unify exchange initialization

--- hummingbot/core/rate_oracle/sources/rate_source_base.py
+++ hummingbot/core/rate_oracle/sources/rate_source_base.py
@@ -9,6 +9,9 @@ from hummingbot.logger import HummingbotLogger
 class RateSourceBase(ABC):
     _logger: Optional[HummingbotLogger] = None
 
+    def __init__(self):
+        self._exchange = None
+
     @property
     @abstractmethod
     def name(self) -> str:
@@ -19,6 +22,13 @@ class RateSourceBase(ABC):
         if cls._logger is None:
             cls._logger = logging.getLogger(__name__)
         return cls._logger
+
+    def _ensure_exchanges(self):
+        if self._exchange is None:
+            self._exchange = self._build_exchange()
+
+    def _build_exchange(self):
+        raise NotImplementedError
 
     @abstractmethod
     async def get_prices(self, quote_token: Optional[str] = None) -> Dict[str, Decimal]:

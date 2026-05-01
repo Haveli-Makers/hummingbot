6091183e1839bbd0f661fb134b29237c9d268ba1
piyush-chittara <piyush.chittara@gmail.com>
2026-05-01 18:29:26 +0530

    init changes?

--- hummingbot/strategy_v2/controllers/__init__.py
+++ hummingbot/strategy_v2/controllers/__init__.py
@@ -1,12 +1,4 @@
 from hummingbot.strategy_v2.controllers.controller_base import ControllerBase, ControllerConfigBase
-from hummingbot.strategy_v2.controllers.directional_trading_controller_base import (
-    DirectionalTradingControllerBase,
-    DirectionalTradingControllerConfigBase,
-)
-from hummingbot.strategy_v2.controllers.market_making_controller_base import (
-    MarketMakingControllerBase,
-    MarketMakingControllerConfigBase,
-)
 
 __all__ = [
     "ControllerBase",
@@ -16,3 +8,29 @@ __all__ = [
     "MarketMakingControllerBase",
     "MarketMakingControllerConfigBase",
 ]
+
+
+def __getattr__(name):
+    if name in {"DirectionalTradingControllerBase", "DirectionalTradingControllerConfigBase"}:
+        from hummingbot.strategy_v2.controllers.directional_trading_controller_base import (
+            DirectionalTradingControllerBase,
+            DirectionalTradingControllerConfigBase,
+        )
+
+        return {
+            "DirectionalTradingControllerBase": DirectionalTradingControllerBase,
+            "DirectionalTradingControllerConfigBase": DirectionalTradingControllerConfigBase,
+        }[name]
+
+    if name in {"MarketMakingControllerBase", "MarketMakingControllerConfigBase"}:
+        from hummingbot.strategy_v2.controllers.market_making_controller_base import (
+            MarketMakingControllerBase,
+            MarketMakingControllerConfigBase,
+        )
+
+        return {
+            "MarketMakingControllerBase": MarketMakingControllerBase,
+            "MarketMakingControllerConfigBase": MarketMakingControllerConfigBase,
+        }[name]
+
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")

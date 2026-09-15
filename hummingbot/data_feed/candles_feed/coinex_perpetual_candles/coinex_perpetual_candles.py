e26d129d7230230c643cf3a2c15a4d8ab151f688
dipti-hm <dipti.m@havelimakers.com>
2026-09-15 12:48:26 +0530

    fix hyperliquid pairs in candles feed

--- hummingbot/data_feed/candles_feed/coinex_perpetual_candles/coinex_perpetual_candles.py
+++ hummingbot/data_feed/candles_feed/coinex_perpetual_candles/coinex_perpetual_candles.py
@@ -23,6 +23,7 @@ class CoinexPerpetualCandles(CandlesBase):
     def __init__(self, trading_pair: str, interval: str = "1m", max_records: int = 150):
         super().__init__(trading_pair, interval, max_records)
         self._polling_task: Optional[asyncio.Task] = None
+        self._historical_fill_task: Optional[asyncio.Task] = None
         self._is_running = False
         self._shutdown_event = asyncio.Event()
         self._historical_fill_in_progress = False
@@ -175,6 +176,14 @@ class CoinexPerpetualCandles(CandlesBase):
                     pass
         self._polling_task = None
         self._is_running = False
+        if self._historical_fill_task and not self._historical_fill_task.done():
+            self._historical_fill_task.cancel()
+            try:
+                await self._historical_fill_task
+            except asyncio.CancelledError:
+                pass
+        self._historical_fill_task = None
+        self._historical_fill_in_progress = False
 
     def _get_rest_candles_params(
         self,
@@ -288,7 +297,7 @@ class CoinexPerpetualCandles(CandlesBase):
             if candles.size > 0:
                 self._candles.extend(candles)
                 self._ws_candle_available.set()
-                safe_ensure_future(self.fill_historical_candles())
+                self._historical_fill_task = safe_ensure_future(self.fill_historical_candles())
                 self.logger().info(
                     f"CoinEx Perpetual candles seeded with {len(self._candles)} recent candles "
                     f"for {self._trading_pair} [{self.interval}]; backfill scheduled."
@@ -352,7 +361,7 @@ class CoinexPerpetualCandles(CandlesBase):
                 if candles:
                     self._candles.append(candles[-1])
                     self._ws_candle_available.set()
-                    safe_ensure_future(self.fill_historical_candles())
+                    self._historical_fill_task = safe_ensure_future(self.fill_historical_candles())
                 return
 
             for candle in candles:
--- hummingbot/data_feed/candles_feed/zebpay_spot_candles/zebpay_spot_candles.py
+++ hummingbot/data_feed/candles_feed/zebpay_spot_candles/zebpay_spot_candles.py
@@ -22,6 +22,7 @@ class ZebpaySpotCandles(CandlesBase):
     def __init__(self, trading_pair: str, interval: str = "1m", max_records: int = 150):
         super().__init__(trading_pair, interval, max_records)
         self._polling_task: Optional[asyncio.Task] = None
+        self._historical_fill_task: Optional[asyncio.Task] = None
         self._is_running = False
         self._shutdown_event = asyncio.Event()
         self._historical_fill_in_progress = False
@@ -193,6 +194,14 @@ class ZebpaySpotCandles(CandlesBase):
                     pass
         self._polling_task = None
         self._is_running = False
+        if self._historical_fill_task and not self._historical_fill_task.done():
+            self._historical_fill_task.cancel()
+            try:
+                await self._historical_fill_task
+            except asyncio.CancelledError:
+                pass
+        self._historical_fill_task = None
+        self._historical_fill_in_progress = False
 
     def _get_rest_candles_params(
         self,
@@ -339,7 +348,7 @@ class ZebpaySpotCandles(CandlesBase):
             if candles.size > 0:
                 self._candles.extend(candles)
                 self._ws_candle_available.set()
-                safe_ensure_future(self.fill_historical_candles())
+                self._historical_fill_task = safe_ensure_future(self.fill_historical_candles())
                 self.logger().info(
                     f"Zebpay candles seeded with {len(self._candles)} recent candles "
                     f"for {self._trading_pair} [{self.interval}]; backfill scheduled."
@@ -407,7 +416,7 @@ class ZebpaySpotCandles(CandlesBase):
                 if candles:
                     self._candles.append(candles[-1])
                     self._ws_candle_available.set()
-                    safe_ensure_future(self.fill_historical_candles())
+                    self._historical_fill_task = safe_ensure_future(self.fill_historical_candles())
                 return
 
             for candle in candles:

770b6d03bc528686858e4edbdff29af8f202cfad
dipti-hm <dipti.m@havelimakers.com>
2026-09-25 11:39:31 +0530

    fix: correct spelling of 'coinswitch' in volume sources mapping

--- hummingbot/core/volume_oracle/volume_oracle.py
+++ hummingbot/core/volume_oracle/volume_oracle.py
@@ -36,7 +36,7 @@ VOLUME_ORACLE_SOURCES = {
     "coindcx": CoindcxVolumeSource,
     "coindcx_perpetual": CoinDCXPerpetualVolumeSource,
     "coinex": CoinexVolumeSource,
-    "coin_switch": CoinswitchVolumeSource,
+    "coinswitch": CoinswitchVolumeSource,
     "csx": CsxVolumeSource,
     "cube": CubeVolumeSource,
     "derive": DeriveVolumeSource,
